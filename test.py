409f53c15105e04ab544959657c2d9403a4e9b9d
Floflew <146342502+Flotauv@users.noreply.github.com>
2025-01-20 14:04:38 +0000

    premiers pas sur streamlit

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
+
 import streamlit as st 
 import pandas as pd 
-import matplotlib.pyplot as plt
+import plotly.express as plx
 
 
+st.title("Dashboard récapitulatif environnement vélo Grenoble")
+st.write("La première version de notre dashboard avec les tenants et les aboutissants de \
+         ce que nous voulons montrer")
 
-st.write("""Salut le monde *test*""")
+@st.cache_data
+def read_csv_file(path):
+    df=pd.read_csv(path,sep=';')
+    return df
 
 
-df_axes_routier = pd.read_csv('')
+#upload_file = st.file_uploader("Choose a file")
+
+df_axes_routier = pd.read_csv('Axes_routiers.csv')
+df_axes_routier = df_axes_routier.drop(columns=['Unnamed: 0'])
+st.dataframe(df_axes_routier)
+
