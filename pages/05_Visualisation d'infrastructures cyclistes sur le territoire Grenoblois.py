579568f332da157c12ba910ac7314f9ca7ae4a20
Floflew <146342502+Flotauv@users.noreply.github.com>
2025-02-01 10:30:03 +0000

    loading packages

--- pages/05_Visualisation d'infrastructures cyclistes sur le territoire Grenoblois.py
+++ pages/05_Visualisation d'infrastructures cyclistes sur le territoire Grenoblois.py
@@ -1,9 +1,39 @@
+import pandas as pd 
 import streamlit as st 
+import osmnx as ox 
+import geopandas as gpd
 
 
 st.title("Visualisation d'infrastructures cyclistes sur le territoire Grenoblois")
 
+## Définition des fonctions utiles à la page : 
 
+# Fonction pour charger les contours des communes
+@st.cache_data
+def load_communes():
+    communes_cibles = [
+        "Grenoble",
+        "Saint-Martin-le-Vinoux",
+        "Fontaine",
+        "Echirolles",
+        "Saint-Martin-d’Hères",
+        "Le Pont-de-Claix",
+        "Eybens",
+        "Poisat",
+        "Seyssinet-Pariset",
+        "Meylan",
+        "La Tronche",
+        "Saint-Egrève"
+    ]
+    communes_selectionnees = gpd.GeoDataFrame()
+    for commune in communes_cibles:
+        try:
+            commune_gdf = ox.geocode_to_gdf(commune + ", France")
+            commune_gdf = commune_gdf.to_crs(epsg=4326)  # CRS compatible avec Folium
+            communes_selectionnees = pd.concat([communes_selectionnees, commune_gdf], ignore_index=True)
+        except Exception as e:
+            st.error(f"Erreur lors du traitement de {commune}: {e}")
+    return gpd.GeoDataFrame(communes_selectionnees, crs="EPSG:4326")
 col1,col2 = st.columns([3,1])
 
 
