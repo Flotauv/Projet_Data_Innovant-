d5c688482eb839f56b83c7468d04851968e49d93
Floflew <146342502+Flotauv@users.noreply.github.com>
2025-02-01 10:09:39 +0000

    checkbox

--- pages/05_Visualisation d'infrastructures cyclistes sur le territoire Grenoblois.py
+++ pages/05_Visualisation d'infrastructures cyclistes sur le territoire Grenoblois.py
@@ -2,3 +2,13 @@ import streamlit as st
 
 
 st.title("Visualisation d'infrastructures cyclistes sur le territoire Grenoblois")
+
+
+col1,col2 = st.columns([3,1])
+
+
+with col1:
+    # Ajouter les boutons pour afficher/masquer les couches
+    show_pistes = st.checkbox("Afficher les pistes cyclables", value=True)
+    show_comptages = st.checkbox("Afficher les nombres de trajets moyen journalier en vélo", value=True)
+    show_arceaux = st.checkbox("Afficher les arceaux pour vélo", value=True)
