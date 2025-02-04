0d4e1d4e3ea354a1628859c80cbeb9e31ee4ddc7
Floflew <146342502+Flotauv@users.noreply.github.com>
2025-02-04 08:31:31 +0000

    page politique vélo

--- pages/03_Politique vélo nationale.py
+++ pages/03_Politique vélo nationale.py
@@ -1,4 +1,12 @@
 import streamlit as st 
+st.set_page_config(page_title='Politiques autour du vélo à Grenoble',layout='centered')
+## Titre
+st.title("Politiques autour du vélo à Grenoble")
 
-st.title("Politique vélo nationale")
-st.write("Cette page présente les grandes lignes de la politique nationale sur le vélo. Ajoutez ici des graphiques ou des données pertinentes.")
+st.write("Cette page présente les mesures phare qui ont pu être prises pour favoriser la politique nationale à propos du vélo dans l'agglomération Grenobloise")
+##Création colonnes
+col_image_principale , col_image_second = st.columns([3,0.1])
+
+with col_image_principale:
+    st.image('Screens/politiques_grenoble.jpeg')
+
