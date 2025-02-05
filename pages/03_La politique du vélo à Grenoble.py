2a913f3b082cf18361a1a9621c86e74573e48bd7
Floflew <146342502+Flotauv@users.noreply.github.com>
2025-02-05 18:10:49 +0000

    graph traffic routier

--- pages/03_La politique du vélo à Grenoble.py
+++ pages/03_La politique du vélo à Grenoble.py
@@ -1,12 +1,87 @@
 import streamlit as st 
+import pandas as pd
+import os
 st.set_page_config(page_title='Politiques autour du vélo à Grenoble',layout='centered')
 ## Titre
 st.title("Politiques autour du vélo à Grenoble")
 
 st.write("Cette page présente les mesures phare qui ont pu être prises pour favoriser la politique nationale à propos du vélo dans l'agglomération Grenobloise")
+
+## Création des fonctions 
+
+### Affichage du traffic routier
+
+#### Chemin du dossier 
+
+repertory_traffic = 'BaseDeDonnées/Traffic_routier/'
+
+#### Fonction qui retourne les axes routiers de Grenoble et leur taux moyens journaliers sur l'année
+def fct_transform_tmja_to_axes_routier(file):
+
+    df_tmja = pd.read_csv(file, sep=None,engine='python')  
+    ##Conversion en date pour extraire l'année
+    df_tmja.columns = df_tmja.columns.str.lower()
+    df_tmja['datereferentiel'] = pd.to_datetime(df_tmja['datereferentiel'])
+    df_tmja['annee'] = df_tmja['datereferentiel'].dt.year
+    
+    ##Filtre sur les routes du département de l'Isère
+    df_tmja = df_tmja[df_tmja['depprd'] == 38]
+    df_tmja = df_tmja.dropna()  # Il y en a très peu
+    
+    df_tmja['longueur'] = df_tmja['longueur'].str.replace(',', '.')
+    df_tmja['ratiopl'] = df_tmja['ratiopl'].str.replace(',', '.')
+    df_tmja['ratiopl'] = df_tmja['ratiopl'].astype(float)
+    
+    df_tmja['ratiopl'] = df_tmja['ratiopl']/100
+    df_tmja['nb_poids_lourds'] = round(df_tmja['tmja'] * df_tmja['ratiopl'])
+    ## Création de la base de donnée avec les routes 
+    df_axes_routier = df_tmja.groupby(['route', 'annee'])[['longueur', 'tmja', 'nb_poids_lourds']].sum().reset_index().sort_values(by='tmja', ascending=False)
+    
+    df_axes_routier['ratiopl'] = round((df_axes_routier['nb_poids_lourds']/df_axes_routier['tmja'])*100)
+    
+    df_axes_routier = df_axes_routier[(df_axes_routier['route'] != 'N0007') & (df_axes_routier['route'] != 'A0007N')]
+    df_axes_routier = df_axes_routier.rename(columns={'tmja': 'Taux Moyen Journalier Annualisé (en Millions)'})
+    
+    df_axes_routier = df_axes_routier.rename(columns={'route': 'Axe routier'})
+
+    df_axes_routier['Axe routier'] = df_axes_routier['Axe routier'].replace('A0041', 'A41')
+    df_axes_routier['Axe routier'] = df_axes_routier['Axe routier'].replace('A0043', 'A43')
+    df_axes_routier['Axe routier'] = df_axes_routier['Axe routier'].replace('A0007', 'A7')
+    df_axes_routier['Axe routier'] = df_axes_routier['Axe routier'].replace('A0048', 'A48')
+    df_axes_routier['Axe routier'] = df_axes_routier['Axe routier'].replace('A0049', 'A49')
+    df_axes_routier['Axe routier'] = df_axes_routier['Axe routier'].replace('N0085', 'Nationale 85')
+    df_axes_routier['Axe routier'] = df_axes_routier['Axe routier'].replace('N0087', 'Nationale 87')
+    df_axes_routier['Axe routier'] = df_axes_routier['Axe routier'].replace('A0051N', 'A51')
+    
+    return df_axes_routier
+
+#### Fonction de concatenation 
+
+def fct_concat():
+    L=[]
+    for file_2 in os.listdir('BaseDeDonnées/Traffic_routier'):
+        if file_2!='.DS_Store':
+            L.append(file_2)
+    df = fct_transform_tmja_to_axes_routier(repertory_traffic+str(L[0]))
+    for i in range(1,len(L)):
+    #df_0 = fct_transform_tmja_to_axes_routier(repertory_traffic+str(L[0]))
+    #df=fct_transform_tmja_to_axes_routier(repertory_traffic+str(L[i]))
+        df = pd.concat([df,fct_transform_tmja_to_axes_routier(repertory_traffic+str(L[i]))])
+
+    return df
+
 ##Création colonnes
 col_image_principale , col_image_second = st.columns([3,0.1])
+col_traffic_principale, col_traffic_second = st.columns([3,0.1])
 
 with col_image_principale:
     st.image('Screens/politiques_grenoble.jpeg')
+
+with col_traffic_principale:
+    st.line_chart(data=fct_concat(),
+                  x='Axe routier',
+                  y='Taux Moyen Journalier Annualisé (en Millions)',
+                  color='annee')
+    
+    
 
