65e7e5ec82294ac5fceec3e419615e90716ff9e5
Floflew <146342502+Flotauv@users.noreply.github.com>
2025-02-06 08:40:19 +0000

    ajout graph traffic

--- pages/03_La politique du vélo à Grenoble.py
+++ pages/03_La politique du vélo à Grenoble.py
@@ -16,11 +16,13 @@ st.write("Cette page présente les mesures phare qui ont pu être prises pour fa
 repertory_traffic = 'BaseDeDonnées/Traffic_routier/'
 
 #### Fonction qui retourne les axes routiers de Grenoble et leur taux moyens journaliers sur l'année
+@st.cache_data()
 def fct_transform_tmja_to_axes_routier(file):
 
     df_tmja = pd.read_csv(file, sep=None,engine='python')  
     ##Conversion en date pour extraire l'année
     df_tmja.columns = df_tmja.columns.str.lower()
+    df_tmja.columns = df_tmja.columns.str.replace(r'_','',regex=True)
     df_tmja['datereferentiel'] = pd.to_datetime(df_tmja['datereferentiel'])
     df_tmja['annee'] = df_tmja['datereferentiel'].dt.year
     
@@ -35,10 +37,11 @@ def fct_transform_tmja_to_axes_routier(file):
     df_tmja['ratiopl'] = df_tmja['ratiopl']/100
     df_tmja['nb_poids_lourds'] = round(df_tmja['tmja'] * df_tmja['ratiopl'])
     ## Création de la base de donnée avec les routes 
-    df_axes_routier = df_tmja.groupby(['route', 'annee'])[['longueur', 'tmja', 'nb_poids_lourds']].sum().reset_index().sort_values(by='tmja', ascending=False)
+    df_axes_routier = df_tmja.groupby(['route', 'annee'])[[ 'tmja', 'nb_poids_lourds']].mean().reset_index().sort_values(by='tmja', ascending=False)
     
     df_axes_routier['ratiopl'] = round((df_axes_routier['nb_poids_lourds']/df_axes_routier['tmja'])*100)
-    
+    df_axes_routier['tmja']=round(df_axes_routier['tmja'])
+    df_axes_routier['annee']=df_axes_routier['annee'].astype(str)
     df_axes_routier = df_axes_routier[(df_axes_routier['route'] != 'N0007') & (df_axes_routier['route'] != 'A0007N')]
     df_axes_routier = df_axes_routier.rename(columns={'tmja': 'Taux Moyen Journalier Annualisé (en Millions)'})
     
@@ -56,7 +59,7 @@ def fct_transform_tmja_to_axes_routier(file):
     return df_axes_routier
 
 #### Fonction de concatenation 
-
+@st.cache_data()
 def fct_concat():
     L=[]
     for file_2 in os.listdir('BaseDeDonnées/Traffic_routier'):
@@ -70,18 +73,25 @@ def fct_concat():
 
     return df
 
+
+
+## Accidents
+
 ##Création colonnes
 col_image_principale , col_image_second = st.columns([3,0.1])
 col_traffic_principale, col_traffic_second = st.columns([3,0.1])
 
 with col_image_principale:
     st.image('Screens/politiques_grenoble.jpeg')
-
+    st.write('Plusieurs mesures phares qui englobent un large périmètre')
 with col_traffic_principale:
-    st.line_chart(data=fct_concat(),
+    st.subheader('Évolution du traffic autour de Grenoble',divider=True)
+    st.bar_chart(data=fct_concat(),
                   x='Axe routier',
                   y='Taux Moyen Journalier Annualisé (en Millions)',
-                  color='annee')
+                  color='annee',
+                  x_label='Axes routiers',
+                  y_label='Tmja (en milliers)')
     
     
 
