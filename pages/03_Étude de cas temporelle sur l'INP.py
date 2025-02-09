d24acc2aab6c08d6b3c9bb67c49cdaaa9b348bd3
OuiOui1234567 <axellaporte747@gmail.com>
2025-02-09 12:00:32 +0100

    Update 03_Étude de cas temporelle sur l'INP.py

--- pages/03_Étude de cas temporelle sur l'INP.py
+++ pages/03_Étude de cas temporelle sur l'INP.py
@@ -1,6 +1,5 @@
 import streamlit as st 
 
-
 # Titres 
-st.title("Etude de cas temporelle sur l'INP")
+st.title("Etude de cas temporelle sur l'INP et l'UGA")
 st.write("Ajoutez ici une étude temporelle détaillée sur l'INP. Vous pouvez inclure des graphiques ou des analyses basées sur les données temporelles.")
