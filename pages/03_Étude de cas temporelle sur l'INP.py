8f1afeee29cee2ca478e387fde031414e1bfaf1a
OuiOui1234567 <axellaporte747@gmail.com>
2025-02-09 18:03:29 +0100

    Update 03_Étude de cas temporelle sur l'INP.py

--- pages/03_Étude de cas temporelle sur l'INP.py
+++ pages/03_Étude de cas temporelle sur l'INP.py
@@ -20,7 +20,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_excel(file_path)
+df = pd.read_excel("BaseDeDonnées/df_merged.xlsx")
 
 # Vérifier si les colonnes nécessaires existent
 required_columns = [
