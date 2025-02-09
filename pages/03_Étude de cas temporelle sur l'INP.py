bcdb530b93074717c94c8c09f94edb968ad7213e
OuiOui1234567 <axellaporte747@gmail.com>
2025-02-09 12:06:23 +0100

    Update 03_Étude de cas temporelle sur l'INP.py

--- pages/03_Étude de cas temporelle sur l'INP.py
+++ pages/03_Étude de cas temporelle sur l'INP.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 # Titres 
 st.title("Etude de cas temporelle sur l'INP et l'UGA")
+st.header("Quelles sont les modes de transport utilisés par les usagers de l'INP (étudiants et personnels) ?")
+st.header("La distance parcourue influence-t-elle le choix du mode de transport ? Que représente chaque moyen de transport dans le kilométrage total ?")
+
 st.write("Ajoutez ici une étude temporelle détaillée sur l'INP. Vous pouvez inclure des graphiques ou des analyses basées sur les données temporelles.")
