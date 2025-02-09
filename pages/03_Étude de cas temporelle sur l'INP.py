d5fd14419b7449e5f7d1e6a0ab84b662dba7c620
OuiOui1234567 <axellaporte747@gmail.com>
2025-02-09 12:13:59 +0100

    Update 03_Étude de cas temporelle sur l'INP.py

--- pages/03_Étude de cas temporelle sur l'INP.py
+++ pages/03_Étude de cas temporelle sur l'INP.py
@@ -2,6 +2,14 @@ import streamlit as st
 
 # Titres 
 st.title("Etude de cas temporelle sur l'INP et l'UGA")
+st.write(
+    """
+Cette page présente une analyse des données de l'INP, groupe d'écoles de l'Université Grenoble Alpes. Les visualisations s'appuient sur une enquête intitulée "Challenge mobilité".
+Cette enquête compare le trajet quotidien d'un usager de l'INP, réalisé tout au long de l'année, en termes de modes de transport utilisés et de kilomètres parcourus sur chaque mode,
+avec le trajet effectué lors d'une journée dédiée au challenge, où l'usager doit tenter d'utiliser des modes de transport écologiques.
+    """
+)
+
 st.header("Quelles sont les modes de transport utilisés par les usagers de l'INP (étudiants et personnels) ?")
 st.header("La distance parcourue influence-t-elle le choix du mode de transport ? Que représente chaque moyen de transport dans le kilométrage total ?")
 
